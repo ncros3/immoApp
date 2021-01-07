09e4fddf3e4bcc99ff10430d4a08b957c51d51f4
ncros <ncros@macbook-pro-de-ncros.home>
2021-01-07 23:08:16 +0100

    data declaration + private functions declaration

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,33 @@
 import realEstate.RealEstateInvest
 
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    val appart: RealEstateInvest = RealEstateInvest()
-    appart.getInfo()
+    val house: Map<String,Double> = mapOf(
+            "houseCost" to 92000.0,
+            "inputCapital" to 18000.0,
+            "rent" to 630.0,
+            "occupationRate" to 0.95
+    )
+
+    val funding: Map<String,Double> = mapOf(
+            "loanRate" to 1.5,
+            "loanDuration" to 30.0
+    )
+
+    val charges: Map<String,Double> = mapOf(
+            "condominiumCost" to 1400.0,
+            "insurance" to 120.0,
+            "electricity" to 900.0,
+            "works" to 800.0,
+            "realEstateTaxes" to 1200.0,
+            "rentManagingCost" to 0.07
+    )
+
+    val taxes: Map<String,Int> = mapOf(
+            "marginalRate" to 30,
+            "taxationRegime" to 1
+    )
+
+    val appart: RealEstateInvest = RealEstateInvest(house, funding, charges, taxes)
+    appart.metrics()
 }
--- src/main/kotlin/realEstate/RealEstateInvest.kt
+++ src/main/kotlin/realEstate/RealEstateInvest.kt
@@ -1,7 +1,28 @@
 package realEstate
 
-class RealEstateInvest {
-    fun getInfo(){
-        println("Hello Real Estate class !")
+class RealEstateInvest(
+        val house: Map<String,Double>,
+        val funding: Map<String,Double>,
+        val charges: Map<String,Double>,
+        val taxes: Map<String,Int>) {
+
+    private fun grossReturn(house: Map<String,Double>){
+        // to implement
+    }
+
+    private fun fundingCalc(funding: Map<String,Double>){
+        // to implement
+    }
+
+    private fun chargesCalc(charges: Map<String,Double>){
+        // to implement
+    }
+
+    fun metrics(){
+        grossReturn(house)
+        fundingCalc(funding)
+        chargesCalc(charges)
+
+        println("Real Estate Invest Metrics")
     }
 }
