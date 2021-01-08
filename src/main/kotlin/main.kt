d9118df3c29a48a348cb4fde2b4a323b0810967a
ncros <ncros@macbook-pro-de-ncros.home>
2021-01-08 07:51:19 +0100

    use data class to create structure

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,15 @@
 import realEstate.RealEstateInvest
+import realEstate.House
+import realEstate.Funding
+import realEstate.Charges
+import realEstate.Taxes
 
 fun main(args: Array<String>) {
 
-    val house: Map<String,Double> = mapOf(
-            "houseCost" to 92000.0,
-            "inputCapital" to 18000.0,
-            "rent" to 630.0,
-            "occupationRate" to 0.95
-    )
-
-    val funding: Map<String,Double> = mapOf(
-            "loanRate" to 1.5,
-            "loanDuration" to 30.0
-    )
-
-    val charges: Map<String,Double> = mapOf(
-            "condominiumCost" to 1400.0,
-            "insurance" to 120.0,
-            "electricity" to 900.0,
-            "works" to 800.0,
-            "realEstateTaxes" to 1200.0,
-            "rentManagingCost" to 0.07
-    )
-
-    val taxes: Map<String,Int> = mapOf(
-            "marginalRate" to 30,
-            "taxationRegime" to 1
-    )
+    val house: House = House(92000, 0, 18000, 630, 0.95)
+    val funding: Funding = Funding(0.015, 30)
+    val charges: Charges = Charges(1400, 120, 900, 800, 1200, 0.07)
+    val taxes: Taxes = Taxes(30, "nude")
 
     val appart: RealEstateInvest = RealEstateInvest(house, funding, charges, taxes)
     appart.metrics()
--- src/main/kotlin/realEstate/RealEstateInvest.kt
+++ src/main/kotlin/realEstate/RealEstateInvest.kt
@@ -1,27 +1,67 @@
 package realEstate
 
+data class House(
+        val houseCost: Int,
+        val works: Int,
+        val inputCapital: Int,
+        val rent: Int,
+        val occupationRate: Double
+)
+
+data class Funding(
+        val loanRate: Double,
+        val loanDuration: Int
+)
+
+data class Charges(
+        val condominiumCost: Int,
+        val insurance: Int,
+        val electricity: Int,
+        val works: Int,
+        val realEstateTaxes: Int,
+        val rentManagingCost: Double
+)
+
+data class Taxes(
+        val marginalRate: Int,
+        val taxationRegime: String
+)
+
+data class HouseOutput(
+        var turnover: Double,
+        var grossReturn: Double
+)
+
 class RealEstateInvest(
-        val house: Map<String,Double>,
-        val funding: Map<String,Double>,
-        val charges: Map<String,Double>,
-        val taxes: Map<String,Int>) {
+        val house: House,
+        val funding: Funding,
+        val charges: Charges,
+        val taxes: Taxes) {
 
-    private fun grossReturn(house: Map<String,Double>){
+    private val notaryCost: Double = house.houseCost * 0.08
+
+    private fun houseCalc(house: House): HouseOutput{
+        // to implement
+        val output: HouseOutput = HouseOutput(
+                house.rent * 12 * house.occupationRate,
+                house.rent * 12 * house.occupationRate / house.houseCost * 100)
+
+        return output
+    }
+
+    private fun fundingCalc(funding: Funding){
         // to implement
     }
 
-    private fun fundingCalc(funding: Map<String,Double>){
-        // to implement
-    }
-
-    private fun chargesCalc(charges: Map<String,Double>){
+    private fun chargesCalc(charges: Charges){
         // to implement
     }
 
     fun metrics(){
-        grossReturn(house)
+        println(houseCalc(house))
         fundingCalc(funding)
         chargesCalc(charges)
+
 
         println("Real Estate Invest Metrics")
     }
